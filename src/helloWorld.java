12575d7c6ff791498cd0aa6e8e409a233363540c
cmagnus <cmagnus@gmail.com>
2016-01-26 14:16:53 -0500

    hello World Constructors finished

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,4 +1,17 @@
 
 public class helloWorld {
 
+	// Variables/Properties
+	String helloString = "hello World";
+	
+	// Constructors
+	public helloWorld() {
+		helloString = "hello World";
+	}
+	
+	public helloWorld(String newString) {
+		helloString = newString;
+	}
+	
+	// Methods
 }
