622b271816039bb4848dc9eafb5503482182d872
cmagnus <cmagnus@gmail.com>
2016-01-28 15:51:59 -0500

    fixed capitalization

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main
   // the main (and only) method
   public static void main(String[] args) 
   {
-    HelloWorld hello = new HelloWorld();
+    HelloWorld hello = new HelloWorld("blahblahblah");
 
-    hello.setHelloString("blahblah");
+    //hello.setHelloString("blahblah");
     String newString = hello.getHelloString();
     
     System.out.println(newString);
