b2a4ffaf4231ffc6f8bddae7ad4dea28f0cb8f4c
cmagnus <cmagnus@gmail.com>
2016-01-28 11:55:36 -0500

    code at end of class Tue. Jan 26, 2016

--- src/test.java
+++ src/test.java
@@ -3,7 +3,10 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		helloWorld hello = new helloWorld();
 
-	}
+		hello.setHelloString("blahblah");
+		String newString = hello.getHelloString();
+;	}
 
 }
